be20ca84de3203b7e81466503fbfecf363185af2
jose soares <jsoares@jose-mac.nwc.newsworld.net>
2010-09-08 13:21:40 -0400

    The new crtic models class

--- critic/models.py
+++ critic/models.py
@@ -1,155 +1,114 @@
+"""
+django-critic models.
+"""
 import datetime
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
-from django.db.models import Avg, Max, Min, Count
+from django.db.models import Count, Avg
 from django.utils.translation import ugettext_lazy as _ 
-from django.core.cache import cache
 
-from critic import settings as csettings
+from critic.modules import method_for_instance
 
-class InstanceManager(models.Manager):
-    def get_user_option(self, instance, obj, user):
+class RatingManager(models.Manager):
+    """
+    The rating manager.
+    """
+    def add(self, obj, usr, opt):
+        """
+        Add/modify a user option.
+        """
+        # Retreive the method and content type for the supplied instance.
+        method = method_for_instance(obj)        
+        ctype = ContentType.objects.get_for_model(obj)
+        
+        # Check if obj and usr exist and if opt is a valid option.
+        if not obj or not usr or not opt in method.options:
+            return None
+        
+        try:
+            # Get the RatingData instance for the supplied arguments
+            data = self.get(content_type__pk=ctype.pk, object_id=obj.pk, 
+                user__pk=usr.pk)
+            # If something was found, and the method allows changes and
+            # if the supplied opt is different then the current option,
+            # then save the new option for the usr.
+            if method.allow_change and data.option != opt:
+                data.option = opt
+                data.save()
+        except RatingData.DoesNotExist:
+            # If no record was found, create a new one
+            data = self.create(content_type=ctype, object_id=obj.pk, user=usr)
+        return data
+        
+    def change(self, obj, usr, opt):
+        """
+        Change the rating. This will simply call the .add(...) method to 
+        make the change.
+        """
+        return self.add(obj, usr, opt)
+
+    def user_option(self, obj, usr):
+        """
+        Returns the option for the supplied user
+        """
+        extra_kwargs = {}
+
+        if not obj or not usr:
+            return None
+        
         ctype = ContentType.objects.get_for_model(obj)
         try:
-            return InstanceOptionData.objects.get(
-                option__instance__pk=instance.pk,
-                user__pk=user.pk,
-                content_type__pk=ctype.pk,
-                object_id=str(obj.pk))
-        except:
+            return self.get(content_type__pk=ctype.pk, object_id=obj.pk,
+                **extra_kwargs).option
+        except RatingData.DoesNotExist:
             return None
-    
-    def get_average(self, instance, obj=None, cached=True):
-        if not isinstance(instance, Instance):
-            raise ValueError
-        
-        key = 'critic.instance.average.%s' % str(instance.pk)
-        
-        ctype_kwargs = {}
-        if obj:
-            ctype = ContentType.objects.get_for_model(obj)
-            ctype_kwargs['content_type__pk'] = ctype.pk
-            ctype_kwargs['object_id'] = str(obj.pk)
-            
-            key = '%s.%s.%s' % (key, str(ctype.pk), str(obj.pk))
-            
-        ret = cache.get(key)
-        if ret and cached:
-            return ret
-        
-        total, total_weight = 0, 0.0
-        for option in instance.instanceoption_set.all():
-            result = InstanceOptionData.objects.filter(
-                option__pk=option.pk,
-                option__instance__pk=instance.pk,
-                **ctype_kwargs).aggregate(
-                    id_count=Count('id'))['id_count']
-
-            total_weight += float(float(result) * float(option.weight))
-            total += result
-        
-        if total == 0:
-            return total
-        ret = float(float(total_weight) / float(total))
-        
-        cache.set(key, ret, csettings.AVERAGE_TIMEOUT)
-        
-        return ret
-        
-    def get_total(self, instance, obj=None, cached=True):
-        
-        if not isinstance(instance, Instance):
-            raise ValueError
-            
-        key = 'critic.instance.total.%s' % str(instance.pk)
-        
-        ret = cache.get(key)
-        if ret and cached:
-            return ret
-        
-        ctype_kwargs = {}
-        if obj:
-            ctype = ContentType.objects.get_for_model(obj)
-            ctype_kwargs['content_type__pk'] = ctype.pk
-            ctype_kwargs['object_id'] = str(obj.pk)
-            
-            key = '%s.%s.%s' % (key, str(ctype.pk), str(obj.pk))
-            
-        ret = InstanceOptionData.objects.filter(
-            option__instance__pk=instance.pk,
-            **ctype_kwargs).count()
-            
-        cache.set(key, ret, csettings.TIMEOUT)
-        
-        return ret
-            
-    def get_options(self, instance):
-        if not isinstance(instance, Instance):
-            raise ValueError
-            
-        key = 'critic.instance.options.%s' % str(instance.pk)
-        ret = cache.get(key)
-        if ret:
-            return ret
-            
-        ret = instance.instanceoption_set.filter(active=True).order_by('order')
-        cache.set(key, ret, csettings.OPTIONS_TIMEOUT)
-        
-        return ret
-
-
-class Instance(models.Model):
-    name = models.CharField(_('Name'), max_length=255, 
-        help_text=_('Name of the instance.'))
-    slug = models.SlugField(_('Slug'), unique=True, 
-        help_text=_('Slugified name.'))
-    description = models.TextField(_('Description'), blank=True)
-    multiple = models.BooleanField(_('Allow Multiple Selections'), default=False, 
-        help_text=_('Will the instance allow mulitple selections by the same user?'))
-    anonymous = models.BooleanField(_('Allow Anonymous Selections'), default=False, 
-        help_text=_('Will the instance allow anonymous users to make a selection?'))
-    created = models.DateTimeField(_('Date Created'), default=datetime.datetime.now)
-    active = models.BooleanField(_('Active'), default=True)
-    
-    objects = InstanceManager()
-    
-    def __unicode__(self):
-        return self.name
-    
-
-class InstanceOption(models.Model):
-    instance = models.ForeignKey(Instance, verbose_name=_('Instance'), 
-        help_text=_('Instance the option belongs too.'))
-    name = models.CharField(_('Value'), max_length=255,
-        help_text=_('The value of the option.'))
-    weight = models.FloatField(default=0.0)
-    order = models.IntegerField(_('Order'), default=1, 
-        help_text=_('Order of the option.'))
-    active = models.BooleanField(_('Acitve'), default=True)
-    
-    def __unicode__(self):
-        return '%s - %s' % (self.instance.name, self.name)
-        
-    class Meta:
-        ordering = ('order', )
         
     
-class InstanceOptionData(models.Model):
-    option = models.ForeignKey(InstanceOption, verbose_name=_('Instance Option'), 
-        help_text=_('The instance option that was selected.'))
-    user = models.ForeignKey(User, verbose_name=_('User'), null=True, 
-        help_text=_('The user that made a selection'))
-    content_type = models.ForeignKey(ContentType, verbose_name=_('Content Type'))
+    def average(self, obj):
+        """
+        Returns the average rating for the supplied object.
+        """
+        ctype = ContentType.objects.get_for_model(obj)
+        return self.objects.filter(content_type__pk=ctype.pk,
+            object_id=obj.pk).aggregate(Avg('option'))['option__avg']
+        
+    def total(self, obj, opt=None):
+        """
+        Returns the total # of ratings for the supplied object. Can also
+        be supplied for a option to get the total for a certain option.
+        """
+        ctype = ContentType.objects.get_for_model(obj)
+        method = method_for_instance(obj)
+        
+        # If a option is specifed, add the option to the query.
+        opt_kwargs = {}
+        if opt in method.options:
+            opt_kwargs = {'option': opt}
+            
+        return self.objects.filter(content_type__pk=ctype.pk, 
+            object_id=obj.pk, **opt_kwargs).aggregate(
+                Count('id'))['id__count']
+        
+    
+class RatingData(models.Model):
+    """
+    User data for a rating. Holds the object in which the user 
+    has made a rating.
+    """
+    user = models.ForeignKey(User, verbose_name=_('User'), null=True)
+    option = models.IntegerField()
+    content_type = models.ForeignKey(ContentType, 
+        verbose_name=_('Content Type'))
     object_id = models.CharField(_('Object ID'), max_length=255)
     content_object = generic.GenericForeignKey('content_type', 'object_id')
-    updated = models.DateTimeField(_('Date Updated'), default=datetime.datetime.now)
+    updated = models.DateTimeField(_('Date Updated'), 
+        default=datetime.datetime.now)
     
-    def __unicode__(self):
-        return '%s - %s - (%s)' % (self.content_object, self.option.instance.name, self.option.name)
-        
-    def save(self, **kwargs):
-        cache.delete('critic.instance.average.%s' % str(self.option.instance.pk))
-        cache.delete('critic.instance.total.%s' % str(self.option.instance.pk))
-        return super(InstanceOptionData, self).save(**kwargs)
+    objects = RatingManager()
+            
+       
+# Build the methods     
+from critic.modules import build_methods
+build_methods()
