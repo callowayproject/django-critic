4d3ecda3872d2ac19d1db1dacf6f7d85af27e81d
jose soares <jsoares@jose-mac.nwc.newsworld.net>
2010-09-08 11:53:43 -0400

    To be removed

--- critic/utils.py
+++
@@ -1,8 +0,0 @@
-from critic import settings as csettings
-
-def get_cookie_key(instance, content_type, object_id):
-    return '%s_CRITIC_%s_%s_%s' % (
-        csettings.CACHE_PREFIX, 
-        instance.pk, 
-        content_type.id, 
-        object_id)
