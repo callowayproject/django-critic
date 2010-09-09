37ea4618d72dafce37cb0410c9cd223979c4ae4d
jose soares <jsoares@jose-mac.nwc.newsworld.net>
2010-09-09 11:16:20 -0400

    Added the new settings for critic

--- critic/settings.py
+++ critic/settings.py
@@ -1,12 +1,14 @@
+"""
+django-critic settings.
+"""
+
 from django.conf import settings
 
-# The average for an instance, t
-AVERAGE_TIMEOUT = getattr(settings, 'CRITIC_AVERAGE_TIMEOUT', 60)
+# Holds the rating definitions.
+RATING_METHODS = getattr(settings, 'CRITIC_RATING_METHODS', [])
 
-TOTAL_TIMEOUT = getattr(settings, 'CRITIC_TOTAL_TIMEOUT', 60)
+# True|False, to allow a user to change their rating.
+ALLOW_CHANGE = getattr(settings, 'CRITIC_ALLOW_CHANGE', False)
 
-OPTIONS_TIMEOUT = getattr(settings, 'CRITIC_OPTION_CACHE_TIMEOUT', 60)
-
-TIMEOUT = getattr(settings, 'CRITIC_TIMEOUT', 60)
-
-CACHE_PREFIX = getattr(settings, 'CRITIC_CACHE_PREFIX', 'PRE')
+# This is the name of the attribute that will be added to the models.
+RATING_ATTR = getattr(settings, 'CRITIC_RATING_ATTRIBUTE', 'ratings')
