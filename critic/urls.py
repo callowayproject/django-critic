8d8a83e2bcb45fd45a69ee688a98c04a4e13c77b
jose soares <jsoares@jose-mac.nwc.newsworld.net>
2010-09-15 13:51:05 -0400

    revamped the urls

--- critic/urls.py
+++ critic/urls.py
@@ -1,11 +1,29 @@
+"""
+django-critic: urls
+"""
+
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('critic.views',
-    url(r'^(?P<instance_id>\d+)/(?P<option_id>\d+)/(?P<content_type_id>\d+)/(?P<object_id>.*)/vote/$', 
-        'make_vote', 
-        name='c_make_vote'),
-    url(r'^(?P<instance_id>\d+)/(?P<content_type_id>\d+)/(?P<object_id>.*)/data/', 
-        'json_data', 
-        name='c_data')
+
+    url(r'^add/$',
+        'add_rating',
+        name='add_rating'),
+        
+    url(r'^render/(?P<content_type_id>\d+)/(?P<object_id>\d+)/$',
+        'render_rating',
+        name='rating_render'),
+        
+    url(r'^user_rating/(?P<content_type_id>\d+)/(?P<object_id>\d+)/$',
+        'user_rating_json',
+        name='user_rating_json'),
+    
+    url(r'^data/(?P<content_type_id>\d+)/(?P<object_id>\d+)/(?P<option>.*)/$',
+        'rating_data_json',
+        name='rating_data_json_by_option'),
+        
+    url(r'^data/(?P<content_type_id>\d+)/(?P<object_id>\d+)/$',
+        'rating_data_json',
+        name='rating_data_json'),
 )
     
