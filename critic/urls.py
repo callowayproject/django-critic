af7efea652490817315ee9b136db296502727cf8
jose soares <jsoares@jose-mac.nwc.newsworld.net>
2010-09-17 11:27:41 -0400

    Fixed up the name of the urls to have critic in the name

--- critic/urls.py
+++ critic/urls.py
@@ -8,22 +8,22 @@ urlpatterns = patterns('critic.views',
 
     url(r'^add/$',
         'add_rating',
-        name='add_rating'),
+        name='critic_add_rating'),
         
     url(r'^render/(?P<content_type_id>\d+)/(?P<object_id>\d+)/$',
         'render_rating',
-        name='rating_render'),
+        name='critic_rating_render'),
         
     url(r'^user_rating/(?P<content_type_id>\d+)/(?P<object_id>\d+)/$',
         'user_rating_json',
-        name='user_rating_json'),
+        name='critic_user_rating_json'),
     
     url(r'^data/(?P<content_type_id>\d+)/(?P<object_id>\d+)/(?P<option>.*)/$',
         'rating_data_json',
-        name='rating_data_json_by_option'),
+        name='critic_rating_data_json_by_option'),
         
     url(r'^data/(?P<content_type_id>\d+)/(?P<object_id>\d+)/$',
         'rating_data_json',
-        name='rating_data_json'),
+        name='critic_rating_data_json'),
 )
     
