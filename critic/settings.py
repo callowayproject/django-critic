b41d88363dc388a359dd9c6342b07380d17cfd1b
jose soares <jsoares@jose-mac.nwc.newsworld.net>
2010-09-17 11:29:26 -0400

    Added another setting for default templateo

--- critic/settings.py
+++ critic/settings.py
@@ -10,5 +10,9 @@ RATING_METHODS = getattr(settings, 'CRITIC_RATING_METHODS', [])
 # True|False, to allow a user to change their rating.
 ALLOW_CHANGE = getattr(settings, 'CRITIC_ALLOW_CHANGE', False)
 
+# The default location of the render template.
+DEFAULT_RENDER_TEMPLATE = getattr(settings, 
+    'CRITIC_DEFAULT_RENDER_TEMPLATE', 'critic/render.html')
+
 # This is the name of the attribute that will be added to the models.
 RATING_ATTR = getattr(settings, 'CRITIC_RATING_ATTRIBUTE', 'ratings')
